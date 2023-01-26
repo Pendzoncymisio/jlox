6f4975896e0ff7a1b8e759de6d31a65cd600ef37
Pendzoncymisio <pendzoncymisio@gmail.com>
2023-01-26 16:21:45 -0500

    Added scoping

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -2,6 +2,9 @@ package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
 import static com.craftinginterpreters.lox.TokenType.*;
 
 public class Parser {
@@ -22,7 +25,7 @@ public class Parser {
     }
 
     private Expr expression() {
-        return equality();
+        return assignment();
     }
 
     private Stmt declaration() {
@@ -37,7 +40,7 @@ public class Parser {
 
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
-
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
         return expressionStatement();
     }
 
@@ -63,6 +66,35 @@ public class Parser {
         Expr expr = expression();
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
+    }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while(!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
+    private Expr assignment() {
+        Expr expr = equality();
+
+        if (match(EQUAL)) {
+            Token equals = previous();
+            Expr value = assignment();
+
+            if (expr instanceof Expr.Variable) {
+                Token name = ((Expr.Variable)expr).name;
+                return new Expr.Assign(name, value);
+            }
+
+            error(equals, "Invalid assignment target.");
+        }
+
+        return expr;
     }
 
     private Expr equality() {
